ad149f7d877288108143094319ab69344ded60bd
ducxinh <ducxinh.dn.201@gmail.com>
2024-11-19 16:33:53 +0700

    Setup structure demo-ecommece

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
 
 abstract class Controller
 {
-    //
+    /**
+     * Response structure json success.
+     *
+     * @param  array  $data  Data return
+     * @param  int  $statusCode  Status code
+     * @return Response response data json
+     */
+    public function responseSuccess($data = [], $statusCode = Response::HTTP_OK): JsonResponse
+    {
+        return response()->json($data, $statusCode);
+    }
 }
