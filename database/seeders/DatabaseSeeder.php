4a96e7293912ed1d45335ccdd4fcbc8105935682
ducxinh <ducxinh.dn.201@gmail.com>
2024-11-19 16:56:50 +0700

    Seeder User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        $userData = [
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+            'email_verified_at' => now(),
+            'password' => Hash::make('secret123!@#'),
+            'remember_token' => Str::random(10),
+        ];
+        User::updateOrCreate(['email' => $userData['email']], $userData);
     }
 }
